ef72c99789ef1a54e1632ae05d7526f5f2571130
Ubuntu <ubuntu@ip-172-31-27-235.ec2.internal>
2015-12-10 06:16:44 +0000

    incorporate crowdsourced data

--- kmeans_songs.py
+++ kmeans_songs.py
@@ -3,10 +3,41 @@ import csv
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 from sklearn.externals import joblib
+from flask.ext.pymongo import PyMongo
+from pymongo import MongoClient
 
 
 # build the song-emotion matrix
 
+#app = Flask(__name__)
+#app.config['MONGO_DBNAME'] = 'primer'
+#mongo = PyMongo(app,config_prefix='MONGO')
+
+client = MongoClient('localhost', 27017)
+db = client.primer
+
+print db.suggestions.find().count()
+cursor = db.suggestions.find()
+
+emotion_mat = []
+suggestion_ids = []
+for data in cursor:
+    suggestion_ids.append(data['_id'])
+    sad = data['emotion']['sad']
+    frustrated = data['emotion']['frustrated']
+    angry = data['emotion']['angry']
+    anxious = data['emotion']['anxious']
+    emotion_mat.append([sad, frustrated, angry, anxious])
+    
+emotion_mat = np.array(emotion_mat)
+# normalization
+emotion_mat = preprocessing.normalize(emotion_mat)
+
+print emotion_mat
+print emotion_mat.shape
+print suggestion_ids
+
+'''
 song_mat = []
 emotion_mat = []
 with open('old_lyrics_emotion.csv', 'rb') as csvfile:
@@ -31,6 +62,7 @@ with open('old_lyrics_emotion.csv', 'rb') as csvfile:
     emotion_mat = preprocessing.normalize(emotion_mat)
     print emotion_mat.shape
     print len(song_mat)
+'''
 
 
 # extract K-Means
@@ -41,16 +73,15 @@ predictions = clf.fit_predict(emotion_mat)
 means = clf.cluster_centers_
 print predictions.shape, means.shape
 
-song_clusters = []
+suggestion_clusters = []
 for c in range(n_clusters):
     print sum(predictions==c)
-    song_clusters.append([song_mat[x] for x in range(len(predictions)) if predictions[x]==c])
+    suggestion_clusters.append([suggestion_ids[x] for x in range(len(predictions)) if predictions[x]==c])
 
-with open('song_clusters.csv', 'wb') as csvfile:
+with open('suggestion_clusters.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile)
-    for song_cluster in song_clusters:
-        writer.writerow(song_cluster)
+    for suggestion_cluster in suggestion_clusters:
+        writer.writerow(suggestion_cluster)
 
-
+print suggestion_clusters
 joblib.dump(clf, 'song_kmeans.pkl')
- 
